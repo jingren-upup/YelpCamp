9474dc55e699a2914b3316b1d87132482febf53b
wangjingren <43458243@qq.com>
2022-04-18 14:10:37 -0400

    add images

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for(let i = 0; i <50;++i){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}` 
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/9998758",
+            description: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id ad libero beatae neque nam quaerat, error consequuntur maiores dolorum, quibusdam obcaecati! Sequi, et impedit debitis placeat officiis deleniti! Culpa, quasi.',
+            price
         })
         await camp.save(); 
     }
